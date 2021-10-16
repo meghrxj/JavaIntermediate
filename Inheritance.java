944c9bbda9cd2457b26e6bf6b24f34992317afd7
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 01:10:06 +0530

    Update Inheritance.java

--- Inheritance.java
+++ Inheritance.java
@@ -1,3 +1,6 @@
 public class Inheritance {
-    
+    public static void main(String[] args){
+
+        System.out.println("We will learn about inheritance from an Example -> 'File name'");
+    }
 }
