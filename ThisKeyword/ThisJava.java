4d793466448b1d69279eece26a4c8a1e6df4f363
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 14:40:12 +0530

    Update ThisJava.java

--- ThisKeyword/ThisJava.java
+++ ThisKeyword/ThisJava.java
@@ -1,9 +1,26 @@
 package ThisKeyword;
 
-public class ThisJava {
-
-    public static void main(String[] args){
-        
-    }
+public class ThisJava{
+        int a;
+        int b;
     
-}
+    public void setData(int a, int b){
+            this.a = a;
+            this.b = b;
+    }
+public static void main(String[] args){
+
+    ThisJava t = new ThisJava();
+    t.setData(4, 3);
+
+    System.out.println(t.a);
+    System.out.println(t.b);
+
+
+
+                                     }
+                    }
+
+
+    
+
