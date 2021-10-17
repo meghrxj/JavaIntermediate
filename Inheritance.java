0cd2cf99c967dd4946f26997c6c2b47a2e1ce819
meghrxj <hello.meghrajjj@gmail.com>
2021-10-17 14:00:52 +0530

    +Inheritance,  Mouse Example, Constructors

--- Inheritance.java
+++ Inheritance.java
@@ -1,6 +1,17 @@
 public class Inheritance {
     public static void main(String[] args){
 
-        System.out.println("We will learn about inheritance from an Example -> 'File name'");
+        //System.out.println("We will learn about inheritance from an Example -> 'MouseExample'");
+
+        Mouse1 m1 = new Mouse1();
+
+        m1.leftClick();
+        m1.rightClick();
+        m1.scrollDown();
+        m1.scrollUp();
+
+        Mouse2 m2 = new Mouse2();
+
+        m2.connect();
     }
 }
